5657faf3f26ba164e5adb6c308f0d3afec551393
Nikita <38846642+NikitaCode@users.noreply.github.com>
2018-11-17 10:20:58 +1100

    Add files via upload
    added file

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+name=input("what is your name?")
+if name=="Nikita":
+    print("ok")
+if not name=="Nikita":
+    print("ok")
+
+password=input("and what is your password?")
+if password=="yeet":
+    print("Hello, Master")
+if not password=="yeet":
+    print("Hello, " + name)
